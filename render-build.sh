67a07930d82ee852788552f19ff402a9ae4c31a5
Archibong <etims567@gmail.com>
2026-01-02 19:28:00 +0100

    Add ffmpeg

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# 1. Download a portable version of FFmpeg
+mkdir -p ffmpeg
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar xJ -C ffmpeg --strip-components 1
 
-# Standard build commands
+# 2. Add FFmpeg to the path so yt-dlp can find it
+export PATH=$PATH:$(pwd)/ffmpeg
+
+# 3. Standard build commands
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
